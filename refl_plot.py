180d5d1a02e8b6de83eebe85dffd189a109af18b
Krishna Sai Chemudupati <39997602+chemudupatiks@users.noreply.github.com>
2019-06-05 09:47:11 -0600

    Add files via upload

--- refl_plot.py
+++ refl_plot.py
@@ -1,3 +1,7 @@
+# author: Krishna Sai Chemudupati
+# date  : May 31, 2019 
+# email : kchemudu@uwyo.edu
+
 import numpy as np 
 import pandas as pd
 import matplotlib
@@ -32,6 +36,36 @@ def onclick(event):
        coordinates.append((x, y))
        time_alt.append((get_time_hms([times[int(x)]]), y))
 
+def plotImage(reflectivity):
+    # Color map configuration
+    cmap = cm.get_cmap('gist_ncar_r') #gist_ncar_r, gist_ncar , and  prism are other colormaps 
+    
+    # Plotting the reflectivity image.
+    fig = plt.figure()
+    plt.imshow(reflectivity.transpose(), cmap = cmap, origin = 'lower')
+    plt.colorbar()
+    #plt.xticks(times, get_time_hms(times))
+    #plt.xlabel('time')
+    #plt.ylabel(i)
+    #plt.legend()
+
+
+    # Establishing connection between the figure and the onClick event. 
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.show()
+
+    # Closing connection. 
+    fig.canvas.mpl_disconnect(cid)
+
+def plotXY(x, y):
+    fig = plt.figure()
+    plt.plot(x, y) 
+
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.show() 
+
+    fig.canvas.mpl_disconnect(cid) 
+
 ### Main script ###
 
 n_args = len(argv)
@@ -54,26 +88,42 @@ else:
     #max_ref = np.max(reflectivity)
     #min_ref = np.min(reflectivity)
 
-    # Color map configuration
-    cmap = cm.get_cmap('gist_ncar_r') #gist_ncar_r, gist_ncar , and  prism are other colormaps 
+    #plotImage(reflectivity)
+
+    # plot reflectivities of closest upper and lower gate
+    altrange = np.array(nc.variables['altrange'][:]) 
+    print(altrange.shape) 
+    print(alt.shape) 
+    print(reflectivity.shape)
+
+    upper_gate_indices = []
+    lower_gate_indices = [] 
+
+    for i in range(len(alt)): 
+       # print(alt[i])
+       temp =  (altrange - alt[i]) 
+       temp[temp> 0] = np.nan
+       #print(np.where(temp == np.nanmax(temp))[0][0])
+       upper_gate.append(reflectivity[i, np.where(temp == np.nanmin(temp))[0][0]])
+    #print(np.array(upper_gate).data)
+    #print(np.array(upper_gate)[np.where(upper_gate != np.nan)])
+    a = reflectivity[:10,:]
+    print(a.shape) 
+    for i in range(a.shape[0]):
+        for j in range(a.shape[1]):
+            print(a[i][j]),
+            print( " "), 
+        print("\n") 
+        
+    plotXY(times, upper_gate)
     
-    # Plotting the reflectivity image.
-    fig = plt.figure()
-    plt.imshow(reflectivity.transpose(), cmap = cmap, origin = 'lower')
-    plt.colorbar()
-    #plt.xticks(times, get_time_hms(times))
-    #plt.xlabel('time')
-    #plt.ylabel(i)
-    #plt.legend()
+    
+ 
 
+    #print(coordinates) 
+    #print(time_alt)
 
-    # Establishing connection between the figure and the onClick event. 
-    cid = fig.canvas.mpl_connect('button_press_event', onclick)
-    plt.show()
-
-    # Closing connection. 
-    fig.canvas.mpl_disconnect(cid)
-
-   
-    print(coordinates) 
-    print(time_alt)
+    #crop = reflectivity[int(coordinates[0][0]):int(coordinates[1][0])+1][:]
+    #print(crop.shape) 
+    #plt.imshow(crop.transpose(), cmap = cmap, origin = 'lower')
+    #plt.show()
